f67431b9e7ea1d36ce1fb333781dbc72c3f35131
Jackson Booth <jacksontbooth@icloud.com>
2019-04-18 14:09:33 +1000

    yeet

--- neural/bot.py
+++ neural/bot.py
@@ -3,7 +3,7 @@ import threading
 from pynput.mouse import Button, Controller as mousecontrol
 from pynput.keyboard import Key, Controller as keyboardcontrol
 
-item = "test PLZ ignore"
+item = "omg u have friends"
 
 
 mouse = mousecontrol()
@@ -13,7 +13,7 @@ mouse.press(Button.left)
 mouse.release(Button.left)
 
 counter = 0
-while(counter < 3):
+while(counter < 2):
     keyboard.type(item)
     keyboard.press(Key.enter)
     keyboard.release(Key.enter)
---
+++ neural/reader.py
@@ -0,0 +1,4 @@
+import time
+import threading
+from pynput.mouse import Button, Controller as mousecontrol
+from pynput.keyboard import Key, Controller as keyboardcontrol
