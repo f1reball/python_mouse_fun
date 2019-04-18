9fe4b84835426fac20b8b6735b6c2d00ca60dbf3
Jackson Booth <jacksontbooth@icloud.com>
2019-04-18 14:35:31 +1000

    pushh

--- neural/reader.py
+++ neural/reader.py
@@ -2,3 +2,6 @@ import time
 import threading
 from pynput.mouse import Button, Controller as mousecontrol
 from pynput.keyboard import Key, Controller as keyboardcontrol
+
+mouse = mousecontrol()
+keyboard = keyboardcontrol()
