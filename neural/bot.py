e8df859199e3e1f431d2e3c2b81a70c182ca67e9
Jackson Booth <jacksontbooth@icloud.com>
2019-04-18 14:17:45 +1000

    save

--- neural/bot.py
+++ neural/bot.py
@@ -3,7 +3,7 @@ import threading
 from pynput.mouse import Button, Controller as mousecontrol
 from pynput.keyboard import Key, Controller as keyboardcontrol
 
-item = "omg u have friends"
+item = "omg u have friends?"
 
 
 mouse = mousecontrol()
@@ -15,6 +15,6 @@ mouse.release(Button.left)
 counter = 0
 while(counter < 2):
     keyboard.type(item)
-    keyboard.press(Key.enter)
-    keyboard.release(Key.enter)
+    #keyboard.press(Key.enter)
+    #keyboard.release(Key.enter)
     counter +=1
